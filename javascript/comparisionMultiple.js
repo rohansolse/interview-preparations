ada9c39d1e5b3d06b77d98164f13c767c471f627
Rohan Solse <rohan.solse@transsight.com>
2025-11-14 10:26:22 +0530

    add JS concepts including data types, mutability, and this keyword

--- javascript/comparisionMultiple.js
+++ javascript/comparisionMultiple.js
@@ -1,9 +1,24 @@
+// let x = {
+//     flag:1,
+//     toString: function(){
+//         return this.flag++;
+//     }
+// }
+
 let x = {
-    flag:1,
-    toString: function(){
-        return this.flag++;
-    }
-}
+  flag: 1,
+  toString() {
+    return this.flag++;
+  },
+};
+
+// let x = {
+//   counter: 0,
+//   valueOf() {
+//     this.counter++;
+//     return this.counter;   // returns 1, then 2, then 3
+//   }
+// }
 
 if (x == 1 && x == 2 && x == 3) {
   console.log("Hello");
